3ff6346876c3a9edee88b5c5b5d51143ed8ec26e
Lautaro Settembrini <lautarosettembrini@hotmail.com>
2023-06-03 12:10:06 -0300

    Reentrega

--- src/app.js
+++ src/app.js
@@ -33,7 +33,10 @@ app.use(express.json());
 
 app.engine(
   "handlebars",
-  exphbs.engine()
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views", "layouts")
+  })
 );
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "handlebars");
