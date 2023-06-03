6ac10779c1d8fc37a902649fd7287b611238270d
Lautaro Settembrini <lautarosettembrini@hotmail.com>
2023-06-03 11:46:31 -0300

    Reentrega2

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,27 +4,27 @@ const productController = {
   index: async function (req, res) {
     if (req.query.limit) {
       const limit = Number(req.query.limit);
-      const result = await productManager.getProducts(limit);
+      const result = await FileManager.getProducts(limit);
       return res.render("products", result);
     } else {
-      const result = await productManager.getProducts();
+      const result = await FileManager.getProducts();
       return res.render("products", { pageTitle: "Productos", ...result });
     }
   },
   show: async function (req, res) {
     const pid = Number(req.params.pid);
-    const result = await productManager.getProductById(pid);
+    const result = await FileManager.getProductById(pid);
     return res.render("productDetail", { pageTitle: result.product.title, ...result });
   },
   update: async function (req, res) {
     const pid = Number(req.params.pid);
     const newValues = req.body;
-    const result = await productManager.updateProduct(pid, newValues);
+    const result = await FileManager.updateProduct(pid, newValues);
     return res.json(result);
   },
   create: async function (req, res) {
     const { title, description, price, thumbnail, code, stock, category, status } = req.body;
-    const result = await productManager.addProduct(
+    const result = await FileManager.addProduct(
       title,
       description,
       price,
@@ -38,7 +38,7 @@ const productController = {
   },
   destroy: async function (req, res) {
     const pid = Number(req.params.pid);
-    const result = await productManager.deleteProduct(pid);
+    const result = await FileManager.deleteProduct(pid);
     return res.json(result);
   }
 };
